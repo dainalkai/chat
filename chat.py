52ec89013bed2b5cf7cfa3c128148508538735ad
dainalkai <120482248+dainalkai@users.noreply.github.com>
2022-12-16 19:31:57 +0800

    Add files via upload

--- chat.py
+++ chat.py
@@ -3,24 +3,31 @@ import os
 #讀取檔案
 def read_file(filename):
 	chat = []
-	name = None
 	with open(filename, 'r', encoding='utf-8-sig') as f:
-		for line in f:
-			if 'Allen' in line:
-				name = line.strip()
-				continue # 繼續
-			elif 'Tom' in line:
-				name = line.strip()
-				continue
-			if name:		
-				chat.append([name, line])	
+		for line in f:		
+			chat.append(line.strip())	
 	return chat
 
+#轉換檔案
+def convert(chat):
+	new = []
+	name = None
+	for line in chat:
+		if 'Allen' in line:
+			name = line.strip()
+			continue # 繼續
+		elif 'Tom' in line:
+			name = line.strip()
+			continue
+		if name:		
+			new.append([name, line])	
+	return new
+	
 #寫入檔案
 def write_file(outputfile, chat):
 	with open(outputfile, 'w', encoding='utf-8') as f:
 		for p in chat:
-			f.write(p[0] + ': ' + p[1] )
+			f.write(p[0] + ': ' + p[1] + '\n')
 
 def main():
 	filename = '075 input.txt'
@@ -29,9 +36,8 @@ def main():
 		print('有找到檔案!!')
 		chat = read_file(filename)
 	else:
-		print('沒有找到檔案.....')				
+		print('沒有找到檔案.....')
+	chat = convert(chat)				
 	write_file(outputfile, chat)
 
 main ()			
-
-
