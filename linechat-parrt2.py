d73ddb1cc1cc6995cdc9dc75328e37ffd57ae7f2
dainalkai <120482248+dainalkai@users.noreply.github.com>
2022-12-17 16:46:50 +0800

    Add files via upload

---
+++ linechat-parrt2.py
@@ -0,0 +1,15 @@
+lines = []
+with open('079 3.txt', 'r', encoding='utf-8-sig') as f:
+	for line in f:
+		lines.append(line.strip())
+	print(lines)
+	
+for line in lines:
+	s = line.split(' ')
+	time = s[0][:5]
+	name = s[0][5:]
+
+print(s)
+print(time)
+print(name)			
+		
